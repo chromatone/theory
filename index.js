4f36a0980c47f91000c86c7caed98c6c2a9e8b7e
davay42 <fond@fondfrukt.ru>
2021-05-13 15:33:44 +0300

    init

---
+++ index.js
@@ -0,0 +1,65 @@
+export const notes = [
+  {
+    name: 'A',
+    pitch: 0,
+  },
+  {
+    name: 'A#',
+    pitch: 1,
+  },
+  {
+    name: 'B',
+    pitch: 2,
+  },
+  {
+    name: 'C',
+    pitch: 3,
+  },
+  {
+    name: 'C#',
+    pitch: 4,
+  },
+  {
+    name: 'D',
+    pitch: 5,
+  },
+  {
+    name: 'D#',
+    pitch: 6,
+  },
+  {
+    name: 'E',
+    pitch: 7,
+  },
+  {
+    name: 'F',
+    pitch: 8,
+  },
+  {
+    name: 'F#',
+    pitch: 9,
+  },
+  {
+    name: 'G',
+    pitch: 10,
+  },
+  {
+    name: 'G#',
+    pitch: 11,
+  },
+]
+
+export const justCents = [
+  0,
+  112,
+  204,
+  316,
+  386,
+  498,
+  590,
+  702,
+  814,
+  884,
+  1017,
+  1088,
+]
